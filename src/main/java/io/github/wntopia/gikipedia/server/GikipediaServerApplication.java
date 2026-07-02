561e63a9249c3ba1879b7979e4a33ce7b2493e05
snowykte0426 <snowykte0426@naver.com>
2026-07-02 14:37:38 +0900

    chore(global): 프로젝트 초기 설정 정리

--- src/main/java/io/github/wntopia/gikipedia/server/GikipediaServerApplication.java
+++ src/main/java/io/github/wntopia/gikipedia/server/GikipediaServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class GikipediaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	static void main(String[] args) {
+		SpringApplication.run(GikipediaServerApplication.class, args);
 	}
 
 }
